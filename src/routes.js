833a2e6fe14d89e142d37ce6ff7eb97041f6973a
gghez <gregory.ghez@gmail.com>
2015-05-12 23:34:34 +0200

    Simplify auto-update, use forever.

--- src/routes.js
+++ src/routes.js
@@ -6,35 +6,9 @@ router.get('/ping', function (req, res, next) {
     res.send('pong');
 });
 
-router.get('/update',function(req, res, next){
-    var child = child_process.spawn('npm', ['install'], {
-        cwd: __dirname + '/../'
-    });
-
-    console.log('Spawn npm install on', __dirname + '/../');
-
-    var stdout = [], stderr = [];
-    child.stdout.on('data', function (buffer) {
-        stdout.push(buffer);
-    });
-    child.stderr.on('data', function (buffer) {
-        stderr.push(buffer);
-    });
-
-    function sanitize(buffers) {
-        return Buffer.concat(buffers).toString().split('\n').filter(function (l) {
-            return !!l;
-        });
-    }
-
-    child.on('close', function () {
-        res.send({
-            stdout: sanitize(stdout),
-            stderr: sanitize(stderr)
-        });
-
-        process.kill(process.pid);
-    });
+router.get('/update', function (req, res, next) {
+    res.sendStatus(204);
+    process.kill(process.pid);
 });
 
 module.exports = router;
