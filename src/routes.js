1bd869bf6e3c2a16895f269702e5981cf2698620
gghez <gregory.ghez@gmail.com>
2015-05-12 23:10:48 +0200

    Autoupdatable service.

--- src/routes.js
+++ src/routes.js
@@ -6,36 +6,30 @@ router.get('/ping', function (req, res, next) {
     res.send('pong');
 });
 
-function spawnMiddleware(cmd, args, options) {
-    return function (req, res, next) {
-        var child = child_process.spawn(cmd, args, options);
-        var stdout = [], stderr = [];
-        child.stdout && child.stdout.on('data', function (buffer) {
-            stdout.push(buffer);
-        });
-        child.stderr && child.stderr.on('data', function (buffer) {
-            stderr.push(buffer);
-        });
+router.get('/update',function(req, res, next){
+    var child = child_process.spawn('npm', ['install']);
+    var stdout = [], stderr = [];
+    child.stdout.on('data', function (buffer) {
+        stdout.push(buffer);
+    });
+    child.stderr.on('data', function (buffer) {
+        stderr.push(buffer);
+    });
 
-        function sanitize(buffers) {
-            return Buffer.concat(buffers).toString().split('\n').filter(function (l) {
-                return !!l;
-            });
-        }
+    function sanitize(buffers) {
+        return Buffer.concat(buffers).toString().split('\n').filter(function (l) {
+            return !!l;
+        });
+    }
 
-        child.on('close', function () {
-            res.send({
-                stdout: sanitize(stdout),
-                stderr: sanitize(stderr)
-            });
+    child.on('close', function () {
+        res.send({
+            stdout: sanitize(stdout),
+            stderr: sanitize(stderr)
         });
 
-        child.unref();
-    };
-}
-
-router.get('/update', spawnMiddleware('npm', ['install']));
-
-router.get('/restart', spawnMiddleware('npm', ['restart'], { detached: true, stdio: 'ignore'}));
+        process.kill(process.pid);
+    });
+});
 
 module.exports = router;
